549d2938cd54f9ce68c0649066d64ac5b3e407db
Emre Yilmaz <emrey@kth.se>
2017-04-13 02:14:42 +0200

    First working version..

--- kaldi_align.sh
+++ kaldi_align.sh
@@ -2,13 +2,15 @@
 
 if [[ $(hostname) == "applejack" ]]; then
     KALDI_main=/vol/customopt/kaldi
+elif [[ $(hostname) == "twist" ]]; then
+    KALDI_main=/vol/tensusers/eyilmaz/kaldi
 else
     echo "Specify KALDI_main!" >&2
     exit 2
 fi
 KALDI_root=$KALDI_main/egs/wsj/s5
-. $KALDI_root/path.sh
-. $KALDI_root/utils/parse_options.sh
+$KALDI_root/path.sh
+$KALDI_root/utils/parse_options.sh
 
 inputdir=$1
 datadir=$2/data
@@ -16,7 +18,10 @@ langdir=$3/lang
 modeldir=$3/AM
 aligndir=$2/align
 outdir=$4
+
 mkdir -p $datadir
+
+cd $KALDI_root
 
 for inputfile in $inputdir/*.wav; do
   file_id=$(basename "$inputfile" .wav)
@@ -27,9 +32,11 @@ for inputfile in $inputdir/*.wav; do
   echo "$file_id $text" > $datadir/text
 
   fbank=${datadir}
-  . $KALDI_root/steps/make_fbank.sh --nj 1 --cmd "run.pl" $fbank $fbank/log $fbank/data || exit 1;
-  . $KALDI_root/steps/compute_cmvn_stats.sh $fbank $fbank/log $fbank/data || exit 1;
-  . $KALDI_root/steps/nnet/align.sh --nj 1 --cmd "run.pl" $datadir $langdir $modeldir $aligndir || exit 1;
-  . $KALDI_root/steps/get_train_ctm.sh $datadir $langdir $aligndir || exit 1;
+  $KALDI_root/steps/make_fbank.sh --fbank_config $modeldir/fbank.conf --nj 1 --cmd "run.pl" $fbank $fbank/log $fbank/data || exit 1;
+  $KALDI_root/steps/compute_cmvn_stats.sh $fbank $fbank/log $fbank/data || exit 1;
+  $KALDI_root/steps/nnet/align.sh --nj 1 --cmd "run.pl" $datadir $langdir $modeldir $aligndir || exit 1;
+  $KALDI_root/steps/get_train_ctm.sh $datadir $langdir $aligndir || exit 1;
   cp $aligndir/ctm $outdir/${file_id}.ctm
 done
+
+cd -
