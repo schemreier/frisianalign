5acc0b2d1828dfda15bee7b8ee38cfa5e5a9462f
Maarten van Gompel <proycon@anaproy.nl>
2020-03-20 13:32:54 +0100

    set scratchdir

--- fame_align/kaldi_align.sh
+++ fame_align/kaldi_align.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 inputdir=$1
-datadir=$2/data
+scratchdir=$2/data
 resourcedir=$3
 langdir=$3/lang
 modeldir=$3/AM
@@ -21,7 +21,7 @@ fatalerror() {
     exit 2
 }
 
-mkdir -p $datadir
+mkdir -p $scratchdir
 mkdir -p $tempdir
 
 cd $resourcedir
@@ -32,17 +32,17 @@ cd $resourcedir
 for inputfile in $inputdir/*.wav; do
   file_id=$(basename "$inputfile" .wav)
   sox $inputfile -e signed-integer -r 16000 -b 16 $tempdir/${file_id}.wav || fatalerror "Failure calling sox"
-  echo "$file_id $tempdir/${file_id}.wav" > $datadir/wav.scp
-  echo "$file_id spk0000" > $datadir/utt2spk
-  echo "spk0000 $file_id" > $datadir/spk2utt
+  echo "$file_id $tempdir/${file_id}.wav" > $scratchdir/wav.scp
+  echo "$file_id spk0000" > $scratchdir/utt2spk
+  echo "spk0000 $file_id" > $scratchdir/spk2utt
   text=$(cat $inputdir/${file_id}.txt)
-  echo "$file_id $text" > $datadir/text
+  echo "$file_id $text" > $scratchdir/text
 
-  fbank=${datadir}
+  fbank=${scratchdir}
   steps/make_fbank.sh --fbank_config $modeldir/fbank.conf --nj 1 --cmd "run.pl" $fbank $fbank/log $fbank/data || fatalerror "Failure running make_fbank.sh"
   steps/compute_cmvn_stats.sh $fbank $fbank/log $fbank/data || fatalerror "Failure running compute_cmvn_stats"
-  steps/nnet/align.sh --nj 1 --cmd "run.pl" $datadir $langdir $modeldir $aligndir || fatalerror "Failure running nnet/align"
-  steps/get_train_ctm.sh $datadir $langdir $aligndir || fatalerror "Failure running get_train_ctm"
+  steps/nnet/align.sh --nj 1 --cmd "run.pl" $scratchdir $langdir $modeldir $aligndir || fatalerror "Failure running nnet/align"
+  steps/get_train_ctm.sh $scratchdir $langdir $aligndir || fatalerror "Failure running get_train_ctm"
   cp $aligndir/ctm $outdir/${file_id}.ctm || fatalerror "Copy failed"
   scripts/ctm2tg.py $outdir/${file_id}.ctm $tempdir//${file_id}.wav || fatalerror "Failure running ctm2tg"
 
