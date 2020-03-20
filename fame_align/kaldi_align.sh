879ce78a3c648b31b01bb43ffbcc881e6471fe19
Maarten van Gompel <proycon@anaproy.nl>
2020-03-20 12:49:05 +0100

    better error handling in kaldi_align script

--- fame_align/kaldi_align.sh
+++ fame_align/kaldi_align.sh
@@ -9,6 +9,18 @@ aligndir=$2/align
 tempdir=$2/temp
 outdir=$4
 
+echo "Input directory: $inputdir" >&2
+echo "Scratch directory: $scratchdir" >&2
+echo "Output directory: $outdir" >&2
+echo "Resource directory: $resourcedir" >&2
+
+fatalerror() {
+    echo "-----------------------------------------------------------------------" >&2
+    echo "FATAL ERROR: $*" >&2
+    echo "-----------------------------------------------------------------------" >&2
+    exit 2
+}
+
 mkdir -p $datadir
 mkdir -p $tempdir
 
@@ -19,7 +31,7 @@ cd $resourcedir
 
 for inputfile in $inputdir/*.wav; do
   file_id=$(basename "$inputfile" .wav)
-  sox $inputfile -e signed-integer -r 16000 -b 16 $tempdir/${file_id}.wav
+  sox $inputfile -e signed-integer -r 16000 -b 16 $tempdir/${file_id}.wav || fatalerror "Failure calling sox"
   echo "$file_id $tempdir/${file_id}.wav" > $datadir/wav.scp
   echo "$file_id spk0000" > $datadir/utt2spk
   echo "spk0000 $file_id" > $datadir/spk2utt
@@ -27,12 +39,12 @@ for inputfile in $inputdir/*.wav; do
   echo "$file_id $text" > $datadir/text
 
   fbank=${datadir}
-  steps/make_fbank.sh --fbank_config $modeldir/fbank.conf --nj 1 --cmd "run.pl" $fbank $fbank/log $fbank/data || exit 1;
-  steps/compute_cmvn_stats.sh $fbank $fbank/log $fbank/data || exit 1;
-  steps/nnet/align.sh --nj 1 --cmd "run.pl" $datadir $langdir $modeldir $aligndir || exit 1;
-  steps/get_train_ctm.sh $datadir $langdir $aligndir || exit 1;
-  cp $aligndir/ctm $outdir/${file_id}.ctm
-  scripts/ctm2tg.py $outdir/${file_id}.ctm $tempdir//${file_id}.wav
+  steps/make_fbank.sh --fbank_config $modeldir/fbank.conf --nj 1 --cmd "run.pl" $fbank $fbank/log $fbank/data || fatalerror "Failure running make_fbank.sh"
+  steps/compute_cmvn_stats.sh $fbank $fbank/log $fbank/data || fatalerror "Failure running compute_cmvn_stats"
+  steps/nnet/align.sh --nj 1 --cmd "run.pl" $datadir $langdir $modeldir $aligndir || fatalerror "Failure running nnet/align"
+  steps/get_train_ctm.sh $datadir $langdir $aligndir || fatalerror "Failure running get_train_ctm"
+  cp $aligndir/ctm $outdir/${file_id}.ctm || fatalerror "Copy failed"
+  scripts/ctm2tg.py $outdir/${file_id}.ctm $tempdir//${file_id}.wav || fatalerror "Failure running ctm2tg"
 
 done
 
